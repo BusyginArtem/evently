4f830eabcb935a9aeb5ea542304b8ba0b44ea7de
ArtemBusyhin <a.busyhin@urancompany.com>
2024-05-13 11:34:29 +0200

    finish project

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,23 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
   const events = await getAllEvents({
-    query: "",
-    page: 1,
+    query: searchText,
+    category,
+    page,
     limit: 6,
-    category: "",
   });
-  const { data = [], totalPages = 1 } = events || {};
 
   return (
     <>
@@ -49,19 +55,18 @@ export default async function Home() {
         </h2>
 
         <div className="flex flex-col w-full gap-5 md:flex-row">
-          {/* <Search />
-          <CategoryFilter /> */}
+          <Search />
+          <CategoryFilter />
         </div>
 
-        <Collection
-          data={data}
+        <Collection 
+          data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
           limit={6}
-          // page={page}
-          page={1}
-          totalPages={totalPages}
+          page={page}
+          totalPages={events?.totalPages}
         />
       </section>
     </>
